be88253153d6a30f3af080c17e965fb674a4e4b7
ngodingbentar <dewaqintoro@gmail.com>
2021-05-25 19:37:03 +0700

    cleanup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { Button, Spinner } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
-import { InputGroup, InputGroupAddon, InputGroupText, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Form, FormGroup, Label, FormText } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input} from 'reactstrap';
 
 export default function HomeScreen() {
   const [loading, setLoading] = useState(false);
@@ -12,15 +11,9 @@ export default function HomeScreen() {
   const [formats, setFormats] = useState([]);
   const [myData, setMyData] = useState([]);
   const [videoYt, setVideoYt] = useState('');
-  const [videoIG, setVideoIG] = useState('');
-  const [format, setFormat] = useState({});
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [platform, setPlatform] = useState("youtube");
 
-  const toggle = () => setDropdownOpen(prevState => !prevState);
-
-  
   const baseUrl = `/api/v1/`
 
   const submit = async () => {
@@ -33,13 +26,11 @@ export default function HomeScreen() {
         const result = await Axios.get(url)
         if(result.data){
           setLoading(false)
-          setVideoIG(result.data.downloadLink)
           window.open(result.data.downloadLink)
         }
         
       } else {
         const url = `${baseUrl}videoInfo?videoURL=${videoYt}`
-        console.log('url', url)
         const result = await Axios.get(url)
         setMyData(result.data)
         setFormats(result.data.formats)
@@ -60,7 +51,6 @@ export default function HomeScreen() {
     try{
       const myurl = `${baseUrl}setname`
       const result = await Axios.post(myurl, {videoName})
-      // console.log('res', result)
       if(result.data === "setname"){
         const url = `${baseUrl}download?videoURL=${videoYt}&itag=${item.itag}`
         window.open(url)
@@ -78,21 +68,9 @@ export default function HomeScreen() {
     setIsEmpty(true)
   }
 
-  const downIg = async () => {
-    const url = 'https://instagram.fsub3-2.fna.fbcdn.net/v/t51.2885-15/e35/190041757_773774123342047_4165319150335122909_n.jpg?tp=1&_nc_ht=instagram.fsub3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=1_hXH0NCAr8AX_kDRt9&edm=AABBvjUBAAAA&ccb=7-4&oh=5b743377f6b3bd2bbb9f398dcd986516&oe=60B29542&_nc_sid=83d603'
-    window.open(url)
-    console.log(url) 
-  }
-
-  const thisPlatform = async (item) => {
-    console.log(item)
-    setPlatform(item)
-  }
-
   return (
     <Container className="mt-16">
       <h3 className="text-center">Online Video Downloader</h3>
-      {/* <button onClick={()=>downIg()}>downIg</button> */}
       <p className="text-center"><a href="https://www.instagram.com/ngodingbentar/" target="_blank">@ngodingbentar</a></p>
       <div className="platform">
         <input
@@ -101,7 +79,7 @@ export default function HomeScreen() {
           name="platform"
           value="youtube"
           checked={platform === "youtube"}
-          onChange={e => thisPlatform(e.currentTarget.value)}
+          onChange={e => setPlatform(e.currentTarget.value)}
         />
         <label htmlFor="youtube">Youtube</label>
         <input
@@ -110,7 +88,7 @@ export default function HomeScreen() {
           name="platform"
           value="instagram"
           checked={platform === "instagram"}
-          onChange={e => thisPlatform(e.currentTarget.value)}
+          onChange={e => setPlatform(e.currentTarget.value)}
         />
         <label htmlFor="instagram">Instagram</label>
       </div>
@@ -169,7 +147,6 @@ export default function HomeScreen() {
           </section>
         )}
       </div>
-      {/* <Button color="danger" onClick={()=> console.log(formats)}>cek</Button> */}
       
     </Container>
   )
